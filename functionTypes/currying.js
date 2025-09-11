e4c347f0840cbc65496e9d31be06317462da8af1
Dhina <you@example.com>
2025-09-11 10:15:48 +0530

    currying - uncurrying

---
+++ functionTypes/currying.js
@@ -0,0 +1,24 @@
+//UnCurrying----
+
+// function add(a,b,c){
+//     console.log(a+b+c);
+
+// }
+// add(10,20,30)
+
+//Currying ------
+
+function add (a) {
+  return function (b) {
+    return function (c) {
+      console.log(a + b + c)
+    }
+  }
+}
+// add(10)(20)(30)
+
+let curry1 = add(100)
+
+let curry2 = curry1(200)
+
+curry2(300);
