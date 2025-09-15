a4bc8edff1bcd1ae2d16a7db36e8138aa3e7459f
Dhina <you@example.com>
2025-09-15 12:57:13 +0530

    Array

---
+++ arrays/arrays.js
@@ -0,0 +1,25 @@
+let flavers = ['Vanila', 'Butterscortch', 'Lavender','chocolate']
+
+// console.log(flavers);
+
+// console.log(flavers[0]);
+// console.log(flavers[1]);
+// console.log(flavers[2]);
+// console.log(flavers[3]);
+
+// console.log(flavers.length-1);
+// console.log(flavers[flavers.length]);
+// console.log(flavers[flavers.length-1]);
+// console.log(flavers[flavers.length-1]);
+// console.log(flavers[flavers.length-1]);
+
+
+// Dense Array $ Sparse Array
+
+let denseArray = [1,2,3,4,5]
+
+
+let sparseArray = [10,20,,40,,60]
+
+let fans = sparseArray
+console.log(fans);
