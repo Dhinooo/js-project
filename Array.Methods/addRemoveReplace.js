f0fa19ae1221e0b15dc8eba74a777d18477f077d
Dhina <you@example.com>
2025-09-23 11:11:33 +0530

    Array-methods

---
+++ Array.Methods/addRemoveReplace.js
@@ -0,0 +1,49 @@
+// pop () - Method
+
+// Remove From Last Element
+
+// let arr = [10,20,30,40]
+
+// arr.pop()
+
+// let fVal = arr.pop()
+
+// console.log(arr);
+// console.log(fVal);
+// ---------------------------------------
+// push() - Method
+
+// Add the element at last 
+// let arr = [10,20,30,40]
+
+// let popVal = arr.pop()
+
+// console.log(popVal,arr);
+
+// arr.push(50,60,70)
+
+// console.log(arr);
+
+// --------------------------------------------
+
+// shift() - Method
+
+// Remove  the first element 
+
+let arr = [20,30,40,50]
+
+let popVal = arr.pop()
+
+console.log(popVal,arr);
+
+arr.push(60,70,80)
+
+console.log(arr);
+
+arr.shift(10)
+
+console.log(arr);
+
+
+
+
---
+++ arrays/deStructuring.js
@@ -0,0 +1,9 @@
+let arr = [10,20,30,40]
+
+// let fVal = arr[0]
+
+// let [a,b,c,d] = arr
+
+let [a,,,d] = arr
+
+console.log(a,d);
