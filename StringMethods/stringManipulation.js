5f959f27d86ed7c0275b577337aaf77f57f04cf6
Dhina <you@example.com>
2025-11-04 18:28:13 +0530

    str-methods

--- StringMethods/stringManipulation.js
+++ StringMethods/stringManipulation.js
@@ -1,8 +1,15 @@
 // charAt
 
-let str = "hellow World"
+// let str = "hellow World"
+
+// console.log( str.charAt(str.length ) );
+
 
 // charCodeAt
+
+// let str1 = 'Javascript'
+// console.log(str1.charCodeAt(1));
+
 
 // concat
 
@@ -13,3 +20,38 @@ let str = "hellow World"
 // lastIndexOf
 
 // repeat 
+
+let newStr = 'javascript'
+
+// console.log(newStr.repeat(2));
+
+//slice 
+
+// let nStr = 'Single Thread'
+
+// console.log(nStr.slice(3));
+// console.log(nStr.slice(3,8));
+// console.log(nStr.slice(-3,-1)); // (-3, -2)
+
+
+// substring $ substr
+
+//   let newStr1 = 'Single Thread'
+
+//   console.log(newStr1.substring(3));
+//   console.log(newStr1.substring(3,8));
+//   console.log(newStr1.substring(-3));
+//   console.log(newStr1.substring(8,0));
+  
+//Split 
+
+// let words = 'My Name is Iron-Man'
+
+// console.log(words.split("" ));
+// console.log(words.split(" ",3 ));
+// console.log(words.split("-", ));
+// console.log(words.split("-", 1));
+
+//startWith
+let newStr1 = 'My Name is Iron-man'
+  
